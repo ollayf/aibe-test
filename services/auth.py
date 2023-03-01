95ad6a1a364f4b5917d5538aad9f0846ec3a0177
ollayf <ngfei25@gmail.com>
2023-03-01 14:23:48 +0800

    Added dynamic authorization

--- services/auth.py
+++ services/auth.py
@@ -1,18 +1,25 @@
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
+USERNAME='shine'
+PASSWORD='shine'
 
 class User(object):
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
+        self.pw_hash = self._generate_pw_hash(self.password)
+
+    def _generate_pw_hash(self, password: str):
+        return generate_password_hash(password, 'pbkdf2:sha256:260000', 18)
+
 
     def __str__(self):
         return "User(id='%s')" % self.id
 
 
 users = [
-    User(1, 'shine', 'pbkdf2:sha256:260000$jh8EnTtgkY07IJsj$8d33ccd601237cc5f9b331df02d62e035b3bff46ccab1408b3d34a035df5912d'), # shine:shine
+    User(1, USERNAME, PASSWORD), # shine:shine
 ]
 
 
@@ -22,7 +29,7 @@ userid_table = {u.id: u for u in users}
 
 def authenticate(username, password):
     user = username_table.get(username, None)
-    if user and check_password_hash(user.password, password):
+    if user and check_password_hash(user.pw_hash, password):
         return user
 
 
